d998b6ef4ff6fdeeb50793881183357a0843dcd2
Joe Rice <jwrice@gmail.com>
2015-05-13 21:09:25 -0400

    Updates to nodemon

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var morgan = require('morgan')
 app.use(morgan('dev'))
 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('server listening');
-
 });
 
 app.get('/', function (req, res) {
   res.send('welcome!');
+});
+
+app.get('/news', function (req, res) {
+  res.send('Nothing to report');
 });

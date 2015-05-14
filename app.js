1d2fe1a38869bfb5898289f9fdebaa4fdf290c01
Joe Rice <jwrice@gmail.com>
2015-05-14 12:46:34 -0400

    Updates to Rendering Template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require( 'express' );
 var app = express();
 var morgan = require('morgan')
+var swig = require('swig')
 
 app.use(morgan('dev'))
 
@@ -10,14 +11,28 @@ var server = app.listen(3000, function () {
   console.log('server listening');
 });
 
+app.engine('html', swig.renderFile)
+//use swig function to actually render html
+app.set('view engine', 'html')
+//sets the default view engine property to html
+app.set('views', __dirname + '/views')
+//diference between using process.cwd() and __dirname?
+swig.setDefaults({ cache: false })
+//caching saves the doc, and only rerenders if data has changed. important in production, but gets in the way for testing. turn this off
+
+
+
+//passes index.html to Express' view engine (Swig). Swig uses data object to populate variables in the template, looping through people. Express then sends HTML doc as response to browser
+
 app.get('/', function (req, res) {
-  res.send('welcome!');
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
-app.get('/news', function (req, res) {
-  res.send('Nothing to report');
-});
+// app.get('/news', function (req, res) {
+//   res.send('Nothing to report');
+// });
 
-app.get('/change' function(req, res){
-	res.send('I made a change.')
-})
+// app.get('/change' function(req, res){
+// 	res.send('I made a change.')
+// })

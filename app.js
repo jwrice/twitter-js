08f6f6409098bfde2ac5bfe6c0bf17ffe8e4ce62
Joe Rice <jwrice@gmail.com>
2015-05-13 20:56:18 -0400

    Set up server

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require( 'express' );
+var app = express();
+var morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('server listening');
+
+});
+
+app.get('/', function (req, res) {
+  res.send('welcome!');
+});
